e3999c6ba1d7ab127a7c7b0f7bc5b3a1cbf41a0b
Tomaž <tomaz.scavnicar@gmail.com>
2020-01-03 09:03:35 +0100

    New entities Field and FieldRecord

---
+++ Fieldr/src/Domain/Entities/Field.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fieldr.Domain.Entities
+{
+    public class Field
+    {
+        public Field() => FieldRecords = new HashSet<FieldRecord>();
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Lat { get; set; }
+        public float Lng { get; set; }
+        public string wktEPSG4326 { get; set; }
+
+        public ICollection<FieldRecord> FieldRecords { get; set; }
+    }
+}
---
+++ Fieldr/src/Domain/Entities/FieldRecord.cs
@@ -0,0 +1,16 @@
+﻿using Fieldr.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fieldr.Domain.Entities
+{
+    public class FieldRecord : AuditableEntity
+    {
+        public long Id { get; set; }
+        public int FieldId { get; set; }
+        public byte[] Photo { get; set; }
+        public string Note { get; set; }
+        public Field Field { get; set; }
+    }
+}
