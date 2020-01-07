7ab048b3c88c50e98abad885fa9fe03e3466e011
Tomaž <tomaz.scavnicar@gmail.com>
2020-01-07 08:46:19 +0100

    Db migration updating field Lat Lng to Double type

---
+++ Fieldr/src/Infrastructure/Persistence/Migrations/20200107074143_FieldLatLngTypeIsDouble.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fieldr.Infrastructure.Persistence.Migrations
+{
+    public partial class FieldLatLngTypeIsDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Lng",
+                table: "Fields",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Lat",
+                table: "Fields",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Lng",
+                table: "Fields",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double));
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Lat",
+                table: "Fields",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
